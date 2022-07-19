ed86450e015185fe8ff8c65142dc5b5bfac0c0f8
NIKHIL RAJ <NIKHIL RAJ@DESKTOP-J9EO0C6>
2022-07-19 17:54:24 +0530

    hello

--- samplemavenprojecr/src/test/java/samplemavenprojecr11/HiiiiiDeep.java
+++ samplemavenprojecr/src/test/java/samplemavenprojecr11/HiiiiiDeep.java
@@ -7,6 +7,10 @@ public class HiiiiiDeep
 	@Test(groups = "smoke")
 	public void hellodee()
 	{
+		String a=System.getProperty("browser");
+		System.out.println(a);
+		String a1=System.getProperty("url");
+		System.out.println(a1);
 		System.out.println("how are you1111");
 		System.out.println("f999");
 		System.out.println("tyubinmjo");
